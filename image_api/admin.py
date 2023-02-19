2fef41a094d5c31367c7066202d950bb16fc2e46
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-02-19 19:21:15 +0100

    Admin - user view created.

--- image_api/admin.py
+++ image_api/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Picture
+
+
+class PictureInline(admin.TabularInline):
+    model = Picture
+    readonly_fields = ("original_image", "small_thumbnail", "medium_thumbnail")
+    extra = 0
+    classes = ["collapse"]
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = ((None, {"fields": ("tier",)}),)
+
+    inlines = [
+        PictureInline,
+    ]
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "tier"),
+            },
+        ),
+    )
+    list_display = ("username", "tier", "is_staff", "is_active")
+
+
+admin.site.register(User, CustomUserAdmin)
