eb40195f2a46f1fc1e7709a0651b946786f2b49f
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-02-20 10:53:27 +0100

    Test for Picture model

--- image_api/tests.py
+++ image_api/tests.py
@@ -1,12 +1,12 @@
+from unittest import TestCase
+
+from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
-from image_api.models import User, Tier
+from image_api.models import User, Tier, Picture
 import tempfile
 from PIL import Image
-from io import BytesIO
-from django.core.files.uploadedfile import InMemoryUploadedFile
-import sys
 
 
 class UserUploadImageTestCase(APITestCase):
@@ -67,3 +67,45 @@ class UserUrlListTestCase(APITestCase):
         self.client.logout()
         response = self.client.get(reverse("user-root"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class PictureTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testuser")
+        self.image_path = "image_api/test_images/auta.jpg"
+
+    def test_create_picture(self):
+        tier = Tier.objects.create(tier_name="Enterprise",
+                                   max_height_small=200,
+                                   max_height_medium=400,
+                                   show_small_thumbnail=True,
+                                   show_medium_thumbnail=True,
+                                   show_original_image=True,
+                                   show_temp_link=True)
+        user = User.objects.create_user(
+            username="user-test",
+            password="pass-test",
+            tier=tier)
+        with open(self.image_path, "rb") as f:
+            image_file = SimpleUploadedFile(
+                "test_image.jpg", f.read(), content_type="image/jpeg"
+            )
+            picture = Picture.objects.create(
+                username=user,
+                original_image=image_file
+            )
+
+        self.assertEqual(picture.username, user)
+
+        original_image_path = picture.original_image.path
+        original_image = Image.open(original_image_path)
+        self.assertEqual(original_image.width, 1600)
+        self.assertEqual(original_image.height, 1066)
+
+        small_thumbnail_path = picture.small_thumbnail.path
+        small_thumbnail = Image.open(small_thumbnail_path)
+        self.assertEqual(small_thumbnail.height, 200)
+
+        medium_thumbnail_path = picture.medium_thumbnail.path
+        medium_thumbnail = Image.open(medium_thumbnail_path)
+        self.assertEqual(medium_thumbnail.height, 400)
