23f50e4d43b209d6c2b2371343d7dad7b4b892e9
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2023-02-23 19:43:26 +0100

    Temporary url serializer added. + various changes

--- image_api/serializers.py
+++ image_api/serializers.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-from image_api.models import User, Picture, Tier
+from image_api.models import User, Picture, Tier, TempUrl
 from rest_framework import serializers
 
 
@@ -26,10 +26,14 @@ class UserSerializer(serializers.ModelSerializer):
         ]
 
 
-class PictureSerializer(serializers.ModelSerializer):
+class PictureUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Picture
-        fields = []
+        fields = ["original_image"]
+        extra_kwargs = {"original_image": {"required": True}}
+
+    def get_picture_pk(self, obj):
+        return obj.pk
 
     def get_small_thumbnail(self, obj):
         return obj.small_thumbnail.url
@@ -42,9 +46,7 @@ class PictureSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         HOST_NAME = settings.HOST_NAME
-        data = super(
-            PictureSerializer, self
-        ).to_representation(instance)
+        data = super(PictureUserSerializer, self).to_representation(instance)
 
         user = instance.user
 
@@ -67,14 +69,17 @@ class PictureSerializer(serializers.ModelSerializer):
             data["original_image"] = original_image
         else:
             data.pop("original_image")
-
         if user.tier.show_temp_link:
-            data.update({"download_org_img": "jakies url"})
-            # todo pass picture id
-           #  f'{HOST_NAME}/downloads/{instance.user}'
+            data.update(
+                {
+                    "get_download_url":
+                        f"{HOST_NAME}/get-url/{self.get_picture_pk(instance)}"
+                }
+            )
         return data
 
 
+# Tier serializer for detail view of each tier.
 class TierSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tier
@@ -90,50 +95,47 @@ class TierSerializer(serializers.HyperlinkedModelSerializer):
         ]
 
 
-class UserUploadImageSerializer(serializers.ModelSerializer):
-
+class PictureStaffSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Picture
-        fields = ["original_image"]
-        extra_kwargs = {"original_image": {"required": True}}
+        fields = [
+            "id",
+            "small_thumbnail",
+            "medium_thumbnail",
+            "original_image",
+        ]
 
-    def get_small_thumbnail(self, obj):
-        return obj.small_thumbnail.url
 
-    def get_original_image(self, obj):
-        return obj.original_image.url
+class TempUrlSerializer(serializers.ModelSerializer):
+    url_duration = serializers.IntegerField(
+        min_value=300,
+        max_value=30000,
+        error_messages={
+            "min_value": "Ensure this value is greater than or equal to 300 "
+            "and less than or equal to 30000.",
+            "max_value": "Ensure this value is greater than or equal to 300 "
+            "and less than or equal to 30000.",
+        },
+    )
+    picture = serializers.CharField(
+        source="picture.original_image",
+        read_only=True
+    )
+    expiration_date = serializers.DateTimeField(
+        format="%Y-%m-%d %H:%M:%S", read_only=True
+    )
+    alias = serializers.CharField(read_only=True)
 
-    def get_medium_thumbnail(self, obj):
-        return obj.medium_thumbnail.url
+    class Meta:
+        model = TempUrl
+        fields = ["picture", "url_duration", "expiration_date", "alias"]
+        extra_kwargs = {"url_duration": {"required": True}}
 
     def to_representation(self, instance):
         HOST_NAME = settings.HOST_NAME
+        data = super(TempUrlSerializer, self).to_representation(instance)
 
-        data = super(
-            UserUploadImageSerializer, self
-        ).to_representation(instance)
+        data["picture"] = "".join([HOST_NAME, "/", data["picture"]])
 
-        user = instance.user
-
-        if user.tier.show_small_thumbnail:
-            small_thumbnail_url = "".join(
-                [HOST_NAME, self.get_small_thumbnail(instance)]
-            )
-            data["small_thumbnail"] = small_thumbnail_url
-
-        if user.tier.show_medium_thumbnail:
-            medium_thumbnail = "".join(
-                [HOST_NAME, self.get_medium_thumbnail(instance)]
-            )
-            data["medium_thumbnail"] = medium_thumbnail
-
-        if user.tier.show_original_image:
-            original_image = "".join(
-                [HOST_NAME, self.get_original_image(instance)]
-            )
-            data["original_image"] = original_image
-        else:
-            data.pop("original_image")
-
+        data["alias"] = "".join([HOST_NAME, "/download/", data["alias"]])
         return data
-
